59db6912c528565e6586905e46026d199ba73a05
yedeng <dengye221@mails.ucas.ac.cn>
2025-11-28 08:57:40 +0800

    Delete model/transunet.py

--- model/transunet.py
+++
@@ -1,4 +0,0 @@
-import segmentation_models_pytorch as smp
-class TransUNet(smp.Unet):
-    def __init__(self):
-        super().__init__(encoder_name='vit_tiny_patch16_224', encoder_weights=None, in_channels=3, classes=1)
